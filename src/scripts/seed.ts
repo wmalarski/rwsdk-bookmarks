d3791ca73e47b8a70a8444e1a9a569f697115bdb
Wojtek Malarski <w.malarski@gmail.com>
2025-07-15 19:27:09 +0200

    packages bumped

--- src/scripts/seed.ts
+++ src/scripts/seed.ts
@@ -1,24 +1,24 @@
 import { defineScript } from "rwsdk/worker";
 
-import { db } from "@/db";
+// import { db } from "@/db";
 
 export default defineScript(async () => {
-  await db.$executeRawUnsafe(`\
-    DELETE FROM User;
-    DELETE FROM sqlite_sequence;
-  `);
+  // await db.$executeRawUnsafe(`\
+  //   DELETE FROM User;
+  //   DELETE FROM sqlite_sequence;
+  // `);
 
-  await db.user.create({
-    data: {
-      createdAt: new Date(),
-      email: "mj@test.com",
-      emailVerified: false,
-      id: "1",
-      image: null,
-      name: "test",
-      updatedAt: new Date(),
-    },
-  });
+  // await db.user.create({
+  //   data: {
+  //     createdAt: new Date(),
+  //     email: "mj@test.com",
+  //     emailVerified: false,
+  //     id: "1",
+  //     image: null,
+  //     name: "test",
+  //     updatedAt: new Date(),
+  //   },
+  // });
 
   console.log("🌱 Finished seeding");
 });
