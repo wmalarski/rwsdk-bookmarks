8bb3fd006910f16a39cf4b241d45c779515f6eb9
Wojtek Malarski <w.malarski@gmail.com>
2025-07-21 22:33:11 +0200

    started bookmark filters

--- src/modules/bookmarks/components/bookmark-filters.tsx
+++ src/modules/bookmarks/components/bookmark-filters.tsx
@@ -1,5 +1,7 @@
 import { IconFilter } from "@intentui/icons";
-import { type ComponentProps, useId } from "react";
+import { useForm } from "@tanstack/react-form";
+import { type ComponentProps, useId, useState } from "react";
+import * as v from "valibot";
 
 import { Button } from "@/components/button";
 import { Checkbox } from "@/components/checkbox";
@@ -15,64 +17,90 @@ import {
 } from "../utils/use-filters-search-params";
 import { BookmarkTagsField } from "./bookmark-tags-field";
 
+const bookmarksFiltersSchema = () => {
+  return v.object({
+    done: v.union([
+      v.literal("all"),
+      v.literal("completed"),
+      v.literal("uncompleted"),
+    ]),
+    query: v.optional(v.string()),
+    tags: v.optional(v.array(v.string())),
+  });
+};
+
+export type BookmarkFiltersData = v.InferOutput<
+  ReturnType<typeof bookmarksFiltersSchema>
+>;
+
 type BookmarkFiltersProps = {
   params: FiltersSearchParams;
   tags: Tag[];
 };
 
 export const BookmarkFilters = ({ params, tags }: BookmarkFiltersProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
   const formId = useId();
+
+  const form = useForm({
+    defaultValues: {},
+    async onSubmit({ value }) {
+      // onSubmit(value);
+    },
+    validators: {
+      onChange: bookmarksFiltersSchema(),
+    },
+  });
 
   const setFiltersSearchParams = useSetFiltersSearchParams();
 
+  const onSubmit: ComponentProps<"form">["onSubmit"] = (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    setFiltersSearchParams(formData);
+
+    setIsOpen(false);
+  };
+
   return (
-    <Modal>
-      <Button intent="secondary" size="sm">
+    <Modal isOpen={isOpen} onOpenChange={setIsOpen}>
+      <Button intent="secondary">
         <IconFilter />
         Filters
       </Button>
       <Modal.Content>
-        {({ close }) => {
-          const onSubmit: ComponentProps<"form">["onSubmit"] = (event) => {
-            event.preventDefault();
-
-            const formData = new FormData(event.currentTarget);
-            setFiltersSearchParams(formData);
-
-            close();
-          };
-
-          return (
-            <>
-              <Modal.Header>Filters</Modal.Header>
-              <form id={formId} onSubmit={onSubmit}>
-                <Checkbox isSelected={params.random === "on"} name="random">
-                  Random
-                </Checkbox>
-                <DoneFilter done={params.done} />
-                <TextField
-                  className="w-full"
-                  id="query"
-                  label="Query"
-                  name="query"
-                  placeholder="Query"
-                  value={params.query ?? ""}
-                />
-                <BookmarkTagsField
-                  onChange={() => void 0}
-                  selectedTags={params["tags[]"]}
-                  tags={tags}
-                />
-              </form>
-              <Modal.Footer>
-                <Modal.Close />
-                <Button form={formId} intent="primary" type="submit">
-                  Save
-                </Button>
-              </Modal.Footer>
-            </>
-          );
-        }}
+        <Modal.Header>
+          <Modal.Title>Filters</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form id={formId} onSubmit={onSubmit}>
+            <Checkbox isSelected={params.random === "on"} name="random">
+              Random
+            </Checkbox>
+            <DoneFilter done={params.done} />
+            <TextField
+              className="w-full"
+              id="query"
+              label="Query"
+              name="query"
+              placeholder="Query"
+              value={params.query ?? ""}
+            />
+            <BookmarkTagsField
+              onChange={() => void 0}
+              selectedTags={params["tags[]"]}
+              tags={tags}
+            />
+          </form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Modal.Close />
+          <Button form={formId} intent="primary" type="submit">
+            Save
+          </Button>
+        </Modal.Footer>
       </Modal.Content>
     </Modal>
   );
